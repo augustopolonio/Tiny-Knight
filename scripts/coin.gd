0d61d30a0d4f902cc6297ab311a04154b221aa67
Augusto Polonio <augustodrp@gmail.com>
2024-05-25 18:15:48 -0300

    Include the coin scene as a Scene Collection in TileSet to use the coins in TileMap

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,8 @@
 extends Area2D
-@onready var game_manager = %GameManager
+#@onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
-	game_manager.add_point()	
+	var game_manager = body.get_parent().get_node("GameManager")
+	game_manager.add_point()
 	animation_player.play("pickup")
